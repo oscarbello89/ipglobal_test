bfda8a309497a3208046fc3946f71f13af54a68d
oscarbello89 <oscarbello89@users.noreply.github.com>
2020-10-07 10:41:14 +0200

    Test

---
+++ 1-prueba-tecnica/app/tests/ApiMovieTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\APIMovie;
+
+class ApiMovieTest extends TestCase
+{
+
+    public function testBasico(): void
+    {
+        $this->assertEquals(3, 3);
+    }
+}
